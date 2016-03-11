9f3b2e90e3b0c9b10d960c8cc3ba465113a88966
Lewis <2berywhph@gmail.com>
2016-03-11 00:50:38 -0800

    update

--- test/index.py
+++ test/index.py
@@ -5,6 +5,9 @@ import os
 import re
 import json
 import time
+from BeautifulSoup import BeautifulSoup as Soup
+import urllib
+
 
 class Indexer(object):
     def __init__(self):
@@ -15,9 +18,12 @@ class Indexer(object):
         self.termCount = 0 
         self.docCount = 0
         self.postingLists = {}              #{termID1:{docID1:5.4553,docID5:0.324},termID2:{docID9:2.023,docID15:1.998}}
+        self.magnitude = {}                 #{doc1:4.5, doc2: 5.5}  #magnitude is the squre of the norm
 
     def get_content(self, pathName):
         try:
+            get_title = urllib.urlopen(pathName)
+            soup = Soup(get_title)
             file = open(pathName, "r")
             html_text = file.read()
             file.close()
@@ -32,14 +38,23 @@ class Indexer(object):
         except:
             print("Could not remove style and js code from the file located in {}".format(pathName))
             return None
-        return html.fromstring(htmlData).text_content()
+        try:
+            title = soup.title.string.encode("utf-8")
+        except:
+            title = ""
+        return html.fromstring(htmlData).text_content() , title
 
-    def parse(self,raw_content):
+
+       
+
+    def parse(self,raw_content, title):
         raw_content.encode("utf-8")
         terms = re.split("[^a-zA-Z0-9]+",raw_content)
+        title_terms = re.split("[^a-zA-Z0-9]+",title)
+        terms.extend(title_terms)
         self.docID2termIDs[self.docCount] = []
         for term in terms:
-            if(len(term) != 0):
+            if(len(term) > 1):
                 term = term.lower()
                 if term not in self.term2termID:
                     self.term2termID[term] = self.termCount
@@ -53,11 +68,11 @@ class Indexer(object):
         jsonFile.close()
         dirpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Html")
         for i in range(len(data)):
-            self.docID2doc[self.docCount] = data[str(i)]["url"].encode("utf-8")
+            self.docID2doc[self.docCount] = [ data[str(i)]["url"].encode("utf-8") , data[str(i)]["file"].encode("utf-8") ]
             filepath = os.path.join(dirpath, data[str(i)]["file"].encode("utf-8"))
-            content = self.get_content(filepath)
-            if content != None:
-                self.parse(content)
+            item = self.get_content(filepath)
+            if item != None:
+                self.parse(item[0], item[1])
 
     def build_postingLists(self):
         for termID in range(self.termCount):
@@ -88,52 +103,41 @@ class Indexer(object):
                 doc_frequency = self.termId2docFre[termID]
                 self.postingLists[termID][docID] = self.WTF(term_frequency) * self.IDF(doc_frequency)
 
+    def build_magnitutide(self):
+    	for termID in self.postingLists:
+    		for docID in self.postingLists[termID]:
+    			if docID not in self.magnitude:
+    				self.magnitude[docID] = 0
+    			self.magnitude[docID] += self.postingLists[termID][docID] ** 2
+
     def write_postingLists(self):
         with open('postingLists.json', 'w') as f:
             json.dump(self.postingLists, f)
-        
-        
-#        with open('postingLists.txt', 'w') as file:
-#        	for termID in self.postingLists:
-#        		file.write(str(termID)+":{")
-#        		for docID in self.postingLists[termID]:
-#        			file.write(str(docID)+":"+str(self.postingLists[termID][docID])+",")
-#        		file.write("}\n")
+
 
     def write_term2termID(self):
         with open('term2termID.json', 'w') as f:
             json.dump(self.term2termID, f)
-        
-#        with open('term2termID.txt', 'w') as file:
-#        	for term in self.term2termID:
-#        		file.write(str(term)+":"+str(self.term2termID[term])+"\n")
+
 
     def write_docID2doc(self):
         with open('docID2doc.json', 'w') as f:
             json.dump(self.docID2doc, f)
 
-#        with open('docID2doc.txt', 'w') as file:
-#        	for doc in self.docID2doc:
-#        		file.write(str(doc)+":"+str(self.docID2doc[doc])+"\n")
 
     def write_docID2termIDs(self):
         with open('docID2termIDs.json', 'w') as f:
             json.dump(self.docID2termIDs, f)
-                
-#        with open('docID2termIDs.txt', 'w') as file:
-#        	for docID in self.docID2termIDs:
-#        		file.write(str(docID)+":[")
-#        		for termID in self.docID2termIDs[docID]:
-#        			file.write(str(termID)+",")
-#        		file.write("]\n")
+
 
     def write_termId2docFre(self):
         with open('termId2docFre.json', 'w') as f:
             json.dump(self.termId2docFre, f)
-        
-#        with open('termId2docFre.txt', 'w') as file:
-#        	for termID in self.termId2docFre:
-#        		file.write(str(termID)+":"+str(self.termId2docFre[termID])+"\n")
+    
+
+    def write_magnitude(self):
+        with open('magnitude.json', 'w') as f:
+            json.dump(self.termId2docFre, f)
 
     def write_indices_to_disk(self):
     	self.write_postingLists()
@@ -141,6 +145,7 @@ class Indexer(object):
     	self.write_docID2doc()
     	self.write_docID2termIDs()
     	self.write_termId2docFre()
+    	self.write_magnitude()
 
 
 if __name__ == "__main__":
@@ -150,9 +155,8 @@ if __name__ == "__main__":
     myIndexer.build_postingLists()
     myIndexer.build_termId2docFre()
     myIndexer.compute_TF_IDF()
+    myIndexer.build_magnitutide()
     myIndexer.write_indices_to_disk()
     #print("--- %s seconds ---" % (time.time() - start_time))
-
-
 
 
